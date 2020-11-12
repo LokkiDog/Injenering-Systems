57444ac79ab1445fec737f1566160b2548d2194b
IzvekovWeb <izvekov.web@yandex.ru>
2020-11-12 21:32:09 +0300

    Свёртана форма заявки

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,35 @@
-// let add = (a,b) => a+b
-// console.log(add(8,3))
+$( document ).ready(function() {
+
+
+// Стилизация кнопки с файлом
+  var inputs = document.querySelectorAll('.btn-file-btn');
+  Array.prototype.forEach.call(inputs, function(input){
+    var label	 = input.nextElementSibling,
+        labelVal = label.innerHTML;
+
+
+        input.addEventListener('change', function(e){
+          var fileName = '';
+          if( this.files && this.files.length > 1 ) {
+            fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+          }
+          else {
+            fileName = e.target.value.split( "\\" ).pop();
+            if(fileName.length > 25){
+              fileName = fileName.substring(0, 15) + "...";
+            }
+            
+            
+          }
+          if( fileName ) {
+            label.querySelector( 'span' ).innerHTML = fileName;
+          }
+          else {
+            label.innerHTML = labelVal;
+          }
+        });
+      });
+// ---------------------------
+
+
+});
